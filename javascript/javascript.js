c0995727c21b63c9ac5dcb42e845746b7b74bd8f
nikhil <nikhilpramod425@gmail.com>
2023-09-27 10:02:11 +0530

    my portfolio

---
+++ javascript/javascript.js
@@ -0,0 +1,36 @@
+// const wordfrequency = (phrase) => {
+
+//     let frequency={}
+//     words =phrase.split(' ')
+
+// for (word of words){
+//     console.log(word)
+
+//     if (word in frequency){
+//         frequency[word] +=1
+//     }else{
+//         frequency[word]=1
+//     }
+// }
+
+
+//     return frequency
+
+    
+// }
+// console.log(wordfrequency('abc nhm abc'))
+
+  
+
+// preloader
+
+
+
+ var loader =document.getElementById("preloader");
+window.addEventListener("load" ,()=>{
+    loader.style.display = "none";
+})
+
+
+
+
